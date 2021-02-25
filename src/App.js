f2f373efed421ca8858f1a402fade1a206a20a49
juanpumpkinpie <pumpkinpiejuan@gmail.com>
2021-02-25 15:59:54 +0100

    new Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Information from "./information";
 import "./styles.scss";
 import Theme from "./theme";
+import Tick from "./tick";
 import Counter from "./use/useReducer";
 
 function App() {
@@ -10,9 +11,11 @@ function App() {
   return (
     <div className="App" onClick={() => setColor("green")} className={color}>
       Cambia mi color {color}
+      {(document.title = color)}
       <Counter />
       <Information />
       <Theme />
+      <Tick />
     </div>
   );
 }
---
+++ src/tick.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Tick extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
