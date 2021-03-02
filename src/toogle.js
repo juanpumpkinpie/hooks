bf484a1dc070433ed4f543abd106b96bff180982
juanpumpkinpie <pumpkinpiejuan@gmail.com>
2021-03-02 11:05:16 +0100

    add localStorage

--- src/toogle.js
+++ src/toogle.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.scss";
 
 export default function Toogle() {
   const [ColorA, setColorB] = useState(false);
-
+  const [value, setValue] = useState(
+    localStorage.getItem("ThevalueEffect") || ""
+  );
   const handleColor = () => {
     setColorB(!ColorA);
+    localStorage.removeItem("ThevalueEffect");
+  };
+
+  useEffect(
+    (event) => {
+      localStorage.setItem("ThevalueEffect", value);
+    },
+    [value]
+  );
+  const onChange = (event) => {
+    setValue(event.target.value);
   };
 
   const count = 1;
 
   return (
     <div className={ColorA ? "head" : "header"} onClick={handleColor}>
-      <p> PWD: dfhadgfjasgfhiaurfghwab fhabskvhbsvnasjndlkj </p>
+      <p> PWD: Yes it change the color and remove the item in LocalStorage </p>
       {count && <h1>Messages: {count}</h1>}
+
+      <fieldset>
+        <legend>LocalStorage</legend>
+        <label for="name">ThevalueEffect:</label>
+        <input value={value} onChange={onChange} />
+        <p>The value for localStorage is: {value}</p>
+      </fieldset>
     </div>
   );
 }
