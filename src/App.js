39224ebea411b4fc57035af68b22a79088854d4c
juanpumpkinpie <pumpkinpiejuan@gmail.com>
2021-03-03 21:38:11 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Information from "./information";
 import "./styles.scss";
 import Theme from "./theme";
 import Tick from "./tick";
 import Toogle from "./toogle";
 import Counter from "./use/useReducer";
+import axios from "./axios";
 
-function App() {
+function App({ fetchURL }) {
   const [color, setColor] = useState("red");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      console.group("Data from API:");
+      console.log(request);
+      console.groupEnd();
+    }
+    fetchData();
+  }, [fetchURL]);
 
   return (
     <div className="App" onClick={() => setColor("green")} className={color}>
